24af0825b5aa56227bf233b4daaf264dbc430a4e
WiktorPWR <wiktor.jankowskiostrowski@globallogic.com>
2026-06-19 13:49:26 +0200

    Interrupt functions for buttons

---
+++ UART/Core/Src/buttons_functions.c
@@ -0,0 +1,67 @@
+#include "buttons_functions.h"
+
+extern TIM_HandleTypeDef htim3;
+
+volatile Button_pressed button_pressed = {0, NULL, 0};
+
+
+/**
+ * @brief  Disables GPIO external interrupts in the NVIC controller.
+ * @details Blocks further interrupt requests from EXTI lines 5-9 and 10-15 to lock out 
+ * bounce noise while a button press is being verified.
+ * @retval None
+ */
+void Interrupts_GPIO_disable(void) {
+    NVIC_DisableIRQ(EXTI9_5_IRQn);
+    NVIC_DisableIRQ(EXTI15_10_IRQn);
+}
+
+/**
+ * @brief  Enables GPIO external interrupts in the NVIC controller.
+ * @details Re-enables handling of interrupt requests from EXTI lines 5-9 and 10-15 
+ * allowing the system to detect new button press events.
+ * @retval None
+ */
+void Interrupts_GPIO_enable(void) {
+    NVIC_EnableIRQ(EXTI9_5_IRQn);
+    NVIC_EnableIRQ(EXTI15_10_IRQn);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+   switch (GPIO_Pin) {
+        case SWT1_Pin:
+            button_pressed.GPIO_Pin = SWT1_Pin;
+            button_pressed.GPIO_Port = SWT1_GPIO_Port;
+            button_pressed.confirmation = 0;
+            break;
+        case SWT2_Pin:
+            button_pressed.GPIO_Pin = SWT2_Pin;
+            button_pressed.GPIO_Port = SWT2_GPIO_Port;
+            button_pressed.confirmation = 0;
+
+            break;
+        case SWT3_Pin:
+            button_pressed.GPIO_Pin = SWT3_Pin;
+            button_pressed.GPIO_Port = SWT3_GPIO_Port;
+            button_pressed.confirmation = 0;
+            break;
+        case SWT4_Pin:
+            button_pressed.GPIO_Pin = SWT4_Pin;
+            button_pressed.GPIO_Port = SWT4_GPIO_Port;
+            button_pressed.confirmation = 0;
+            break;
+        case SWT5_Pin:
+            button_pressed.GPIO_Pin = SWT5_Pin;
+            button_pressed.GPIO_Port = SWT5_GPIO_Port;
+            button_pressed.confirmation = 0;
+            break;
+        default:
+            break;
+   }
+
+   Interrupts_GPIO_disable();
+
+   HAL_TIM_Base_Start_IT(&htim3);
+}
+
