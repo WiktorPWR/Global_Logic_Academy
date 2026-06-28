a4283163b4ea0114084a3e339d65e9b6e7cac406
WiktorPWR <wiktor.jankowskiostrowski@globallogic.com>
2026-06-28 02:36:17 +0200

    First process uart function implementation

--- I2C/Core/Src/UART.c
+++ I2C/Core/Src/UART.c
@@ -6,19 +6,46 @@ extern UART_HandleTypeDef huart3;
 volatile uint8_t uart3_rx_buffer[UART_RX_BUFFER_SIZE];
 volatile uint8_t uart3_rx_index = 0;
 
-void process_uart3_data(uint8_t *data, uint16_t size, uint8_t *overflow_data, uint8_t overflow_size) {
-    // Implement your data processing logic here
-    // For example, you can print the received data or parse it
+
+HAL_StatusTypeDef process_uart_data(uint8_t *data, uint16_t size, uint8_t *overflow_data, uint8_t overflow_size) {
+    if(data == NULL){
+        return HAL_ERROR;
+    }
+
+    if(size == 0){
+        return HAL_ERROR;
+    }
+
+    if(overflow_data == NULL){
+        // Process the data normally
+        //For now we just simply send a echo back the received data
+        HAL_UART_Transmit(&huart3, data, size, HAL_MAX_DELAY);
+        return HAL_OK;
+    }else{
+        uint16_t full_data_size = size + overflow_size;
+        char full_data[full_data_size + 1]; // +1 for null terminator
+    
+        memcpy(full_data, data, size);
+        
+        if(overflow_size > 0){
+            memcpy(full_data + size, overflow_data, overflow_size);
+        }
+
+        full_data[full_data_size] = '\0'; // Null terminate the string
+
+        HAL_UART_Transmit(&huart3, (uint8_t *)full_data, full_data_size, HAL_MAX_DELAY);
+
+        return HAL_OK;
+    }
 }
 
-HAL_UARTEx_ReceiveToIdle_DMA(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size){
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size){
     if(huart == &huart3){
-        
         if(uart3_rx_index > Size){
             uint8_t overflow_size = UART_RX_BUFFER_SIZE - uart3_rx_index;
-            process_uart3_data(uart3_rx_buffer + uart3_rx_index, overflow_size, uart3_rx_buffer, Size);
+            process_uart_data((uint8_t *)(uart3_rx_buffer + uart3_rx_index), overflow_size, (uint8_t *)uart3_rx_buffer, Size);
         }else{
-            process_uart3_data(uart3_rx_buffer + uart3_rx_index, Size - uart3_rx_index, NULL, NULL);
+            process_uart_data((uint8_t *)(uart3_rx_buffer + uart3_rx_index), Size - uart3_rx_index, NULL, 0);
             
         }
         if(Size == UART_RX_BUFFER_SIZE){
